779d0a8f16368a47d02d80ce23551c92d9cc3894
Thomas Crowe <wtcrowe4@outlook.com>
2024-07-22 13:33:38 -0400

    implements sidebar, hardcoded user

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,30 @@
+import Sidebar from '../../components/ui/Sidebar';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn: User = {
+    $id: '123',
+    email: 'john.doe@example.com',
+    userId: '456',
+    dwollaCustomerUrl: 'https://example.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    dwollaCustomerId: '',
+    address1: '',
+    city: '',
+    state: '',
+    postalCode: '',
+    dateOfBirth: '',
+    ssn: ''
+  };
+
   return (
-    <main>
-        sidebar
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+import { sidebarLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
+
+const Sidebar = ({ user }: SidebarProps) => {
+
+  const pathname = usePathname()
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/"
+          className="mb-12 cursor-pointer items-center gap-2">
+            <Image src='/icons/logo.svg' width={50} height={50} alt='My Finance Logo' 
+              className="size-[24px] max-xl:size-14" />
+            <h1 className="sidebar-logo">My Finance</h1>
+        </Link>
+      
+        {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+          return (
+            <Link href={item.route} key={item.label}
+              className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+            >
+              <div className="relative size-6">
+                <Image 
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    'brightness-[3] invert-0': isActive
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          )
+        })}
+        
+        {/* <PlaidLink user={user} /> */}
+      </nav>
+
+      {/* <Footer user={user} /> */}
+    </section>
+  )
+}
+
+export default Sidebar
