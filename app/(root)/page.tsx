673b0f5bb243367a86d9b7b2e674a73c2a17c882
Thomas Crowe <wtcrowe4@outlook.com>
2024-08-22 07:10:11 -0400

    refactor: Update Home component to include recent transactions and account details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,49 +1,69 @@
 import Header from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalance from '@/components/TotalBalanceBox'
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
-  const loggedIn: User = { 
-    $id: '123',
-    email: 'john.doe@example.com',
-    userId: '456',
-    dwollaCustomerUrl: 'https://example.com',
-    firstName: 'John',
-    lastName: 'Doe',
-    dwollaCustomerId: '',
-    address1: '',
-    city: '',
-    state: '',
-    postalCode: '',
-    dateOfBirth: '',
-    ssn: ''
-  }
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  // const loggedIn: User = { 
+  //   $id: '123',
+  //   email: 'john.doe@example.com',
+  //   userId: '456',
+  //   dwollaCustomerUrl: 'https://example.com',
+  //   firstName: 'John',
+  //   lastName: 'Doe',
+  //   dwollaCustomerId: '',
+  //   address1: '',
+  //   city: '',
+  //   state: '',
+  //   postalCode: '',
+  //   dateOfBirth: '',
+  //   ssn: ''
+  // }
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
+  const currentPage = Number(page as string) || 1;
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+  const account = await getAccount({ appwriteItemId })
+
+  if(!accounts) return;
+
 
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
+          
           <Header 
             type="greeting"
             title="Welcome, "
             user={loggedIn?.firstName || 'Guest'}
-            subtext="Access and manage all your finances with ease."
-          />
+            subtext="Access and manage all your finances with ease." />
+          
           <TotalBalance 
             accounts={[]}
             totalBanks={3}
-            totalCurrentBalance={10000}
-          />
+            totalCurrentBalance={10000} />
+        
         </header>
 
-        Recent Transactions
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage} />
+      
       </div>
 
       <RightSidebar 
         user={loggedIn}
         transactions={[]}
-        banks={[]}
-        />
+        banks={[]} />
+    
     </section>
   )
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -87,7 +87,7 @@ declare type Bank = {
   accessToken: string;
   fundingSourceUrl: string;
   userId: string;
-  sharableId: string;
+  shareableId: string;
 };
 
 declare type AccountTypes =
@@ -312,7 +312,7 @@ declare interface createBankAccountProps {
   accountId: string;
   bankId: string;
   fundingSourceUrl: string;
-  sharableId: string;
+  shareableId: string;
 }
 
 declare interface getBanksProps {
